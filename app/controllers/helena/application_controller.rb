331234f16f66a1b0ffa1cde1aac51cc2d517ac87
Markus Graf <info@markusgraf.ch>
2014-02-17 09:23:39 +0100

    refactoring

--- app/controllers/helena/application_controller.rb
+++ app/controllers/helena/application_controller.rb
@@ -1,4 +1,9 @@
 module Helena
   class ApplicationController < ActionController::Base
+    def authenticate_administrator!
+      unless can_administer?
+        raise Helena::AccessDenied.new("cannot administer")
+      end
+    end
   end
 end
