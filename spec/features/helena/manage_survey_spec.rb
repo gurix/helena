2b9a4e7c8a08e0cb40638a51b807ad8b5f23a888
Markus Graf <info@markusgraf.ch>
2014-02-19 07:51:53 +0100

    added view and tests

--- spec/features/helena/manage_survey_spec.rb
+++ spec/features/helena/manage_survey_spec.rb
@@ -1,15 +1,23 @@
 require 'spec_helper'
 
-feature 'Surveys' do
-  scenario 'List surveys' do
+feature 'Survey management' do
+  scenario 'lists all surveys' do
     create :survey, name: 'My first survey'
     create :survey, name: 'Another cool survey'
 
     visit helena.surveys_path
 
-    within 'table.surveys' do
-      expect(page).to have_text 'My first survey'
-      expect(page).to have_text 'Another cool survey'
+    within '#helena_survey_1' do
+      expect(page).to have_text '1 My first survey'
+    end
+
+    within '#helena_survey_2' do
+      expect(page).to have_text '2 Another cool survey'
     end
   end
+
+  scenario 'creates a new surveys'
+  scenario 'edits a survey'
+  scenario 'delets a survey'
+
 end
