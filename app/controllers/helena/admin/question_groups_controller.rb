14941a36b156e785c7ae12ca1ba7a8cd712bb9ee
Markus Graf <info@markusgraf.ch>
2014-03-02 14:15:31 +0100

    fixed typo

--- app/controllers/helena/admin/question_groups_controller.rb
+++ app/controllers/helena/admin/question_groups_controller.rb
@@ -5,7 +5,7 @@ module Helena
     class QuestionGroupsController < Admin::ApplicationController
       respond_to :html
 
-      before_filter :load_survey, :add_breadcrumps
+      before_filter :load_survey, :add_breadcrumbs
       before_filter :resort, only: [:move_up, :move_down, :create]
 
       def index
@@ -85,7 +85,7 @@ module Helena
         @survey = Helena::Survey.find params['survey_id']
       end
 
-      def add_breadcrumps
+      def add_breadcrumbs
         add_breadcrumb Helena::Survey.model_name.human(count: 2), :admin_surveys_path
         add_breadcrumb @survey.name, admin_survey_question_groups_path(@survey)
       end
