9bc2d502cb2513a1e055abaf24d3c7c7dcb5cdb0
Markus Graf <info@markusgraf.ch>
2014-03-05 19:37:08 +0100

    enriched seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,12 +27,27 @@ restaurant_survey = create :survey, name: 'Restaurant customer satisfaction', de
 
 personal_details = create :question_group, survey: restaurant_survey, title: 'Personal Details', position: 1
 
-personal_details.questions << create(:short_text_question, code: :name, question_text: "What's your name?", position: 1, code: 'name')
-personal_details.questions << create(:short_text_question, code: :email, question_text: "What's your E-Mail-Address?", position: 2)
+personal_details.questions << create(:short_text_question, code:           :name,
+                                                           question_text:  "What's your name?",
+                                                           position:       1,
+                                                           code:           'name',
+                                                           survey:         restaurant_survey,
+                                                           question_group: personal_details)
+personal_details.questions << create(:short_text_question, code: :email,
+                                                           question_text:  "What's your E-Mail-Address?",
+                                                           position:       2,
+                                                           survey:         restaurant_survey,
+                                                           question_group: personal_details)
 
 dinner = create :question_group, survey: restaurant_survey, title: 'About the dinner', position: 2
 dinner.questions << create(:short_text_question, code:             :catchphrase,
                                                  question_text:    'How would you describe the dinner with one word?',
                                                  validation_rules: { presence: true },
                                                  position:         1,
-                                                 survey:           restaurant_survey)
+                                                 survey:           restaurant_survey,
+                                                 question_group:   dinner)
+dinner.questions << create(:long_text_question, code:             :feedback,
+                                                question_text:    'Feel free to give us additional feedback ...',
+                                                position:         2,
+                                                survey:           restaurant_survey,
+                                                question_group:   dinner)
