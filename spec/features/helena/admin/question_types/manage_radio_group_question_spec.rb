57d5008e9bfb93b133f1413aa196e897c8a00b5b
Markus Graf <info@markusgraf.ch>
2014-03-06 17:00:15 +0100

    added tests

---
+++ spec/features/helena/admin/question_types/manage_radio_group_question_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+feature 'Radio group question management' do
+  scenario 'edits a question' do
+    question = create :radio_group_question
+    question.labels << create(:label, text: 'Male', value: 'm', position: 1)
+
+    visit helena.edit_admin_survey_question_group_question_path(question.question_group.survey, question.question_group, question)
+
+
+    check 'Required'
+
+    fill_in 'question_labels_attributes_0_position', with: '2'
+    fill_in 'question_labels_attributes_0_text', with: 'Female'
+    fill_in 'question_labels_attributes_0_value', with: 'f'
+
+    click_button 'Save'
+
+    expect(question.reload.labels.first.position).to eq 2
+    expect(question.reload.labels.first.text).to eq 'Female'
+    expect(question.reload.labels.first.value).to eq 'f'
+    expect(question.reload.required).to be_true
+  end
+
+  scenario 'adds a an option' do
+    question = create :radio_group_question
+
+    visit helena.edit_admin_survey_question_group_question_path(question.question_group.survey, question.question_group, question)
+
+    fill_in 'question_labels_attributes_0_position', with: '2'
+    fill_in 'question_labels_attributes_0_text', with: 'Female'
+    fill_in 'question_labels_attributes_0_value', with: 'f'
+
+    click_button 'Save'
+
+    expect(question.reload.labels.first.position).to eq 2
+    expect(question.reload.labels.first.text).to eq 'Female'
+    expect(question.reload.labels.first.value).to eq 'f'
+  end
+
+  scenario 'removes an option' do
+    question = create :radio_group_question
+    question.labels << create(:label, text: 'Male', value: 'm', position: 1)
+
+    visit helena.edit_admin_survey_question_group_question_path(question.question_group.survey, question.question_group, question)
+
+    check 'question_labels_attributes_0__destroy'
+
+    click_button 'Save'
+
+    expect(question.reload.labels).to be_empty
+  end
+end
