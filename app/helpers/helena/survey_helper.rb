34a389fe2033ca77dab7eceb3eb66740e46054bc
Markus Graf <info@markusgraf.ch>
2014-03-22 18:13:16 +0100

    using sub directories for translation, fixes #1

--- app/helpers/helena/survey_helper.rb
+++ app/helpers/helena/survey_helper.rb
@@ -1,12 +1,7 @@
 module Helena
   module SurveyHelper
     def question_type_translation_for(question_type)
-      question_type_name = "#{question_type}"
-
-      if question_type_name.present?
-        question_type_name = question_type_name.split('::').last.underscore
-        translate "question_types.#{question_type_name}"
-      end
+      question_type.to_s.constantize.model_name.human if question_type.present?
     end
   end
 end
--- lib/helena/engine.rb
+++ lib/helena/engine.rb
@@ -1,5 +1,6 @@
 module Helena
   class Engine < ::Rails::Engine
     isolate_namespace Helena
+    config.i18n.load_path += Dir[config.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
