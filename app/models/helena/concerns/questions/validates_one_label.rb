4e3fd584ad2fd6ba43f7b81ac48de434e2f65a15
Markus Graf <info@markusgraf.ch>
2014-03-23 22:08:52 +0100

    refactoring for the sake of code climate

---
+++ app/models/helena/concerns/questions/validates_one_label.rb
@@ -0,0 +1,23 @@
+module Helena
+  module Concerns
+    module Questions
+      module ValidatesOneLabel
+        extend ActiveSupport::Concern
+
+        included do
+          validate :labels_preselection
+        end
+
+        private
+
+        def labels_preselection
+          selected_labels = labels.select { |label| label.preselected == true }
+          if selected_labels.size > 1
+            selected_labels.each { |label| label.errors.add(:preselected, I18n.t(:taken, scope: 'activerecord.errors.messages')) }
+            errors.add(:labels, I18n.t('helena.admin.radio_group.only_one_preselection_allowed'))
+          end
+        end
+      end
+    end
+  end
+end
--- app/models/helena/questions/radio_matrix.rb
+++ app/models/helena/questions/radio_matrix.rb
@@ -3,18 +3,7 @@ module Helena
     class RadioMatrix < Helena::Question
       include Helena::Concerns::Questions::Validatable
       include Helena::Concerns::Questions::Requirable
-
-      validate :labels_preselection
-
-      private
-
-      def labels_preselection
-        selected_labels = labels.select { |label| label.preselected == true }
-        if selected_labels.size > 1
-          selected_labels.each { |label| label.errors.add(:preselected, I18n.t(:taken, scope: 'activerecord.errors.messages')) }
-          errors.add(:labels, I18n.t('helena.admin.radio_matrix.only_one_preselection_allowed'))
-        end
-      end
+      include Helena::Concerns::Questions::ValidatesOneLabel
     end
   end
 end
