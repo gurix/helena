70f22cbc2950d361abea860297be90fe89cc4f16
Markus Graf <info@markusgraf.ch>
2014-03-27 10:30:22 +0100

    sample seeds for RadioMatrix

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,11 +17,32 @@ puts 'Seeding surveys ...'.green
 def create_satisfaction_survey
   satisfaction_survey = create :survey, name: 'The Satisfaction with Life Scale'
   satisfaction_survey_base_version = satisfaction_survey.versions.create version: 0
-  satisfaction_survey_base_version.survey_detail = create(:survey_detail, title:       'The Satisfaction with Life Scale',
+  satisfaction_survey_base_version.survey_detail = create :survey_detail, title:       'The Satisfaction with Life Scale',
                                                                           description: 'A 5-item scale designed to measure global cognitive judgments of ones life satisfaction.',
-                                                                          version:     satisfaction_survey_base_version)
+                                                                          version:     satisfaction_survey_base_version
 
-  create :question_group, version: satisfaction_survey_base_version
+  satisfaction_details = create :question_group, version: satisfaction_survey_base_version
+
+  satisfaction_matrix = satisfaction_details.questions.create code:           :satisfaction,
+                                                              question_text:  'Below are five statements with which you may agree or disagree. Using the 1-7 scale below, indicate your agreement with each item by placing the appropriate number in the line preceding that item. Please be open and honest in your responding.',
+                                                              validation_rules: { presence: true },
+                                                              position:       1,
+                                                              version:        satisfaction_survey_base_version,
+                                                              type:           'Helena::Questions::RadioMatrix'
+
+  satisfaction_matrix.labels.create position: 1, text: 'Strongly Disagree', value: 1
+  satisfaction_matrix.labels.create position: 2, text: 'Disagree', value: 2
+  satisfaction_matrix.labels.create position: 3, text: 'Slightly Disagree', value: 3
+  satisfaction_matrix.labels.create position: 4, text: 'Neither Agree or Disagree', value: 4
+  satisfaction_matrix.labels.create position: 5, text: 'Slightly Agree', value: 5
+  satisfaction_matrix.labels.create position: 6, text: 'Agree', value: 6
+  satisfaction_matrix.labels.create position: 7, text: 'Strongly Agree', value: 7
+
+  satisfaction_matrix.sub_questions.create text: 'In most ways my life is close to my ideal.', code: 'life_is_ideal', position: 1
+  satisfaction_matrix.sub_questions.create text: 'The conditions of my life are excellent.', code: 'condition', position: 2
+  satisfaction_matrix.sub_questions.create text: 'I am satisfied with life.', code: 'satisfied_with_life', position: 3
+  satisfaction_matrix.sub_questions.create text: 'So far I have gotten the important things I want in life.', code: 'important_things', position: 4
+  satisfaction_matrix.sub_questions.create text: 'If I could live my life over, I would change almost nothing.', code: 'nothing_to_change', position: 5
 end
 
 def create_restaurant_survey
@@ -42,7 +63,6 @@ EOF
   personal_details.questions.create code:           :name,
                                     question_text:  "What's your name?",
                                     position:       1,
-                                    code:           'name',
                                     version:        restaurant_survey_base_version,
                                     type:           'Helena::Questions::ShortText'
 
@@ -54,11 +74,11 @@ EOF
                                     type:           'Helena::Questions::ShortText'
 
   visit_interval = personal_details.questions.create code:           :visit_interval,
-                                                      question_text:  'How often do you visit the Swiss Chees Restaurant?',
-                                                      position:       3,
-                                                      version:        restaurant_survey_base_version,
-                                                      question_group: personal_details,
-                                                      type:           'Helena::Questions::RadioGroup'
+                                                     question_text:  'How often do you visit the Swiss Chees Restaurant?',
+                                                     position:       3,
+                                                     version:        restaurant_survey_base_version,
+                                                     question_group: personal_details,
+                                                     type:           'Helena::Questions::RadioGroup'
 
   visit_interval.labels.create position: 1, text: 'Just once', value: 1
   visit_interval.labels.create position: 2, text: 'Once a year', value: 2
