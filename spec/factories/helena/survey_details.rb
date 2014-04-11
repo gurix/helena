ea36d97a13655cda9d02c897edbbf543c3182c5a
Markus Graf <info@markusgraf.ch>
2014-04-11 17:03:28 +0200

    additional tests for Helena::VersionPublisher

--- spec/factories/helena/survey_details.rb
+++ spec/factories/helena/survey_details.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :survey_detail, class: Helena::SurveyDetail do
     sequence(:title)  { |n| "Survey #{n}, #{Faker::Lorem.words(3).join(' ')}?" }
-    sequence(:description) { Faker::Lorem.paragraphs 3 }
+    sequence(:description) {  Faker::Lorem.paragraphs(3).join(' ') }
   end
 end
