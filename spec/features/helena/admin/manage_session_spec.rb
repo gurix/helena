887046e545f97d8d56ba8e2b83c2f213afc3ec5f
Markus Graf <info@markusgraf.ch>
2014-04-15 20:18:40 +0200

    makeup

--- spec/features/helena/admin/manage_session_spec.rb
+++ spec/features/helena/admin/manage_session_spec.rb
@@ -5,20 +5,20 @@ feature 'Session management' do
   background do
     @survey = create :survey
     baseversion = @survey.versions.create version: 0
-    @version = Helena::VersionPublisher.publish(baseversion)
+    @version = Helena::VersionPublisher.publish baseversion
   end
 
   scenario 'lists all sessions of a survey' do
     session_not_completed = @survey.sessions.create version: @version, token: 'abc'
     session_completed = @survey.sessions.create version: @version, token: 'xyz', completed: true
 
-    visit helena.admin_survey_sessions_path(@survey)
+    visit helena.admin_survey_sessions_path @survey
 
-    within "#helena_#{dom_id(session_not_completed)}" do
+    within "#helena_#{dom_id session_not_completed}" do
       expect(page).to have_text 'less than a minute abc No'
     end
 
-    within "#helena_#{dom_id(session_completed)}" do
+    within "#helena_#{dom_id session_completed}" do
       expect(page).to have_text 'less than a minute xyz Yes'
     end
 
@@ -31,10 +31,10 @@ feature 'Session management' do
   scenario 'deletes a version' do
     session = @survey.sessions.create
 
-    visit helena.admin_survey_sessions_path(@survey)
+    visit helena.admin_survey_sessions_path @survey
 
     within "#helena_#{dom_id(session)}" do
-      expect { click_link 'Delete' }.to change { @survey.sessions.count }.by(-1)
+      expect { click_link 'Delete' }.to change { @survey.sessions.count }.by -1
     end
   end
 end
--- spec/features/helena/admin/manage_version_spec.rb
+++ spec/features/helena/admin/manage_version_spec.rb
@@ -8,15 +8,15 @@ feature 'Version management' do
   end
 
   scenario 'lists all versions of a survey' do
-    published_version = Helena::VersionPublisher.publish(@baseversion)
+    published_version = Helena::VersionPublisher.publish @baseversion
     published_version.notes = 'bla bla'
     published_version.save
 
     visit helena.admin_survey_versions_path(@survey)
 
-    expect(page).not_to have_selector "#helena_#{dom_id(@baseversion)}" # Base version is always the working version
+    expect(page).not_to have_selector "#helena_#{dom_id @baseversion }" # Base version is always the working version
 
-    within "#helena_#{dom_id(published_version)}" do
+    within "#helena_#{dom_id published_version }" do
       expect(page).to have_text '1 bla bla less than a minute'
     end
 
@@ -27,7 +27,7 @@ feature 'Version management' do
   end
 
   scenario 'publishing a new version on the base of base version' do
-    visit helena.admin_survey_versions_path(@survey)
+    visit helena.admin_survey_versions_path @survey
 
     fill_in 'Notes', with: 'Luke, I am your father!'
 
@@ -35,19 +35,20 @@ feature 'Version management' do
 
     published_version = Helena::Version.last
 
-    within "#helena_#{dom_id(published_version)}" do
+    within "#helena_#{dom_id published_version }" do
       expect(page).to have_text '1 Luke, I am your father! less than a minute'
     end
   end
 
   scenario 'deletes a version' do
-    published_version = Helena::VersionPublisher.publish(@baseversion)
+    published_version = Helena::VersionPublisher.publish @baseversion
     published_version.notes = 'bla bla'
     published_version.save
 
-    visit helena.admin_survey_versions_path(@survey)
-    within "#helena_#{dom_id(published_version)}" do
-      expect { click_link 'Delete' }.to change { @survey.versions.count }.by(-1)
+    visit helena.admin_survey_versions_path @survey
+
+    within "#helena_#{dom_id published_version }" do
+      expect { click_link 'Delete' }.to change { @survey.versions.count }.by -1
     end
   end
 end
