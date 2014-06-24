316cd226a1573c8d57afae6b3f9bf81d6d1a3e73
Markus Graf <info@markusgraf.ch>
2014-06-24 10:20:54 +0200

    fixed false require

--- lib/helena.rb
+++ lib/helena.rb
@@ -5,6 +5,7 @@ require 'mongoid'
 require 'mongoid_orderable'
 require 'mongoid-simple-tags'
 require 'liquid'
+require 'csv'
 
 module Helena
 end
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -5,7 +5,6 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'sprockets/railtie'
 require 'rails/mongoid'
-require 'csv'
 
 Bundler.require(*Rails.groups)
 require 'helena'
