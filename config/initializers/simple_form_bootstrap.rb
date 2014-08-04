658d0d95c29d7691b7451f0e2300a83af43fa94b
Markus Graf <info@markusgraf.ch>
2014-08-04 12:43:15 +0200

    fixed missing mapping for boolean values

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -4,6 +4,8 @@ SimpleForm.setup do |config|
   config.button_class = 'btn btn-default'
   config.boolean_label_class = nil
 
+  config.wrapper_mappings =  { boolean: :vertical_boolean }
+
   config.wrappers :vertical_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
