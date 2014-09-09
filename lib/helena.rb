b6ef838fe73de51da966554c8f8323b933e3c86d
Markus Graf <info@markusgraf.ch>
2014-09-09 11:39:15 +0200

    temporary add simple_form as dependency

--- lib/helena.rb
+++ lib/helena.rb
@@ -2,6 +2,7 @@ require 'helena/engine'
 require 'haml'
 require 'helena/version_publisher'
 require 'mongoid'
+require 'simple_form' # TODO: Remove this when amdin interface is finally moved
 require 'mongoid_orderable'
 require 'mongoid-simple-tags'
 require 'csv'
