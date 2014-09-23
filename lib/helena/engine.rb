a77160416f9ba924e984515f44eb58aa3f41c85a
Markus Graf <info@markusgraf.ch>
2014-09-23 13:16:15 +0200

    makeup

--- lib/helena/engine.rb
+++ lib/helena/engine.rb
@@ -4,7 +4,7 @@ module Helena
     config.i18n.load_path += Dir[config.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     # Thanks to https://github.com/thoughtbot/factory_girl_rails/pull/42
-    initializer "helena.factories", :after => "factory_girl.set_factory_paths" do
+    initializer 'helena.factories', after: 'factory_girl.set_factory_paths' do
       FactoryGirl.definition_file_paths << File.expand_path('../../../spec/factories', __FILE__) if defined?(FactoryGirl)
     end
   end
