aae8c6b338fb6710798d1cf8212724cf81190f85
Markus Graf <info@markusgraf.ch>
2014-09-29 14:28:29 +0200

    fixed not published questions

--- lib/helena/version_publisher.rb
+++ lib/helena/version_publisher.rb
@@ -3,7 +3,17 @@ module Helena
     def self.publish(version)
       copied_version = version.dup
       copied_version.survey = version.survey
+
       copied_version.question_groups << version.question_groups
+
+      copied_version.question_groups.each_with_index do |question_group, index|
+        question_group.questions << version.question_groups[index].questions
+      end
+
+      adjust_copied_attributes(version, copied_version)
+    end
+
+    def self.adjust_copied_attributes(version, copied_version)
       copied_version.version = newest_version_of(version.survey) + 1
       copied_version.created_at = DateTime.now
       copied_version.updated_at = DateTime.now
