a130fd3266e067eea31a80f735ae0c492084efe8
Markus Graf <info@markusgraf.ch>
2014-10-13 10:07:42 +0200

    remove more admin things

--- app/controllers/helena/application_controller.rb
+++ app/controllers/helena/application_controller.rb
@@ -1,15 +1,5 @@
 module Helena
   class ApplicationController < ::ApplicationController
     helper Helena::Engine.helpers
-
-    helper_method :can_administer?
-
-    def authenticate_administrator
-      fail(ActionController::RoutingError, 'Access Denied') unless can_administer?
-    end
-
-    def add_breadcrumb(name, path = nil, options = {})
-      super(name.presence || t('shared.untitled'), path, options)
-    end
   end
 end
--- app/models/helena/concerns/questions/validates_one_label.rb
+++ app/models/helena/concerns/questions/validates_one_label.rb
@@ -14,7 +14,7 @@ module Helena
           selected_labels = labels.select { |label| label.preselected == true }
           return if selected_labels.size <= 1
           selected_labels.each { |label| label.errors.add(:preselected, I18n.t(:taken, scope: 'activerecord.errors.messages')) }
-          errors.add(:labels, I18n.t('helena.admin.radio_group.only_one_preselection_allowed'))
+          errors.add(:labels, I18n.t('helena.admin.radio_group.only_one_preselection_allowed')) # TODO: How to manage this translation?
         end
       end
     end
