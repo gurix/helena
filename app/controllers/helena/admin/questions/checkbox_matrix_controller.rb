780e3cfe4afc037cf1f57ed73d08d8bb0bcfcc14
Markus Graf <info@markusgraf.ch>
2014-10-13 09:58:34 +0200

    remove all admin stuff

--- app/controllers/helena/admin/questions/checkbox_matrix_controller.rb
+++
@@ -1,9 +0,0 @@
-module Helena
-  module Admin
-    module Questions
-      class CheckboxMatrixController < Admin::QuestionsController
-        include Helena::Concerns::Questions::MatrixQuestions
-      end
-    end
-  end
-end
