6c65cbe0d613aa091c00cbb1a028d785d385c186
Markus Graf <info@markusgraf.ch>
2014-10-14 11:43:42 +0200

    test csv export

--- spec/models/helena/session_spec.rb
+++ spec/models/helena/session_spec.rb
@@ -17,4 +17,19 @@ describe Helena::Session do
     expect_any_instance_of(Helena::Session).to receive(:generate_token).exactly(2).times.and_return('a493oP')
     expect(create(:session_without_token).token).to eq 'a493oP'
   end
+
+  it 'exports as CSV with answers' do
+    survey = create :survey
+    version = survey.versions.create
+
+    session = survey.sessions.create version: version
+    session.answers << Helena::Answer.build_generic('a', 42, '192.168.0.1')
+    session.answers << Helena::Answer.build_generic('b', true, '192.235.0.1')
+    session.answers << Helena::Answer.build_generic('c', 'Barbra Streisand!!', '192.999.0.1')
+
+    csv = Helena::Session.to_csv
+
+    expect(csv).to include 'a,b,c'
+    expect(csv).to include '42,true,Barbra Streisand!!'
+  end
 end
