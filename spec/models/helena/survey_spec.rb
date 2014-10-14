9a03a2483b278a27459e9be8f5968e06238285aa
Markus Graf <info@markusgraf.ch>
2014-10-14 11:04:54 +0200

    removable functionality not necessary atm

--- spec/models/helena/survey_spec.rb
+++ spec/models/helena/survey_spec.rb
@@ -11,9 +11,4 @@ describe Helena::Survey do
   it 'has a valid factory' do
     expect(build :survey).to be_valid
   end
-
-  it 'is removable by default via Helena::Concerns::ApplicationModel.removable?' do
-    survey = create :survey
-    expect { survey.delete }.to change { Helena::Survey.count }.by(-1)
-  end
 end
