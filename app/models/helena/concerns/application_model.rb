dcb8f22e738f41f9a91f74c1acdced0bdc7b69c1
Markus Graf <info@markusgraf.ch>
2014-11-07 10:20:28 +0100

    we don't cast, we should extend our gem instead

--- app/models/helena/concerns/application_model.rb
+++ app/models/helena/concerns/application_model.rb
@@ -5,8 +5,6 @@ module Helena
 
       include Mongoid::Document
       include Mongoid::Timestamps
-      # We need dynamic attributes here to be able to extend our models in combination with the administration gem
-      include Mongoid::Attributes::Dynamic
     end
   end
 end
