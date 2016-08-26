0ef381010517a37e0d4a55fc38e7014aea21b807
Markus Graf <info@markusgraf.ch>
2016-08-26 15:38:13 +0200

    refactored factories

--- spec/factories/helena/sessions.rb
+++ spec/factories/helena/sessions.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :session, class: Helena::Session do
     sequence(:token)  { |n| "token_#{n}" }
+    survey
+    version
   end
 
   factory :session_without_token, class: Helena::Session do
+    survey
+    version
   end
 end
