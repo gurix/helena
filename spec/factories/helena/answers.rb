594d4885dea4b417ea2d5e8f632777bedac65597
Markus Graf <info@markusgraf.ch>
2017-11-01 16:26:19 +0100

    removed dependencies from sessions

--- spec/factories/helena/answers.rb
+++ spec/factories/helena/answers.rb
@@ -2,27 +2,23 @@ FactoryBot.define do
   factory :answer, class: Helena::Answer do
     sequence(:code) { |n| "answer_#{n}" }
     sequence(:ip_address) { Faker::Internet.ip_v4_address }
-    session
   end
 
   factory :string_answer, class: Helena::StringAnswer do
     sequence(:code)  { |n| "string_answer_#{n}" }
     sequence(:value) { Faker::Lorem.word }
     sequence(:ip_address) { Faker::Internet.ip_v4_address }
-    session
   end
 
   factory :integer_answer, class: Helena::IntegerAnswer do
     sequence(:code)  { |n| "integer_answer_#{n}" }
     sequence(:value) { rand 99 }
     sequence(:ip_address) { Faker::Internet.ip_v4_address }
-    session
   end
 
   factory :boolean_answer, class: Helena::BooleanAnswer do
     sequence(:code)  { |n| "boolean_answer_#{n}" }
     sequence(:value) { [true, false].sample }
     sequence(:ip_address) { Faker::Internet.ip_v4_address }
-    session
   end
 end
