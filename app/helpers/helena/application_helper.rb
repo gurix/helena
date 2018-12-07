3f4dcebad7fdcafcdc35922beca69c8f9daf6ede
Markus Graf <info@markusgraf.ch>
2018-12-07 15:44:35 +0100

    more accessible long text

--- app/helpers/helena/application_helper.rb
+++ app/helpers/helena/application_helper.rb
@@ -1,4 +1,21 @@
 module Helena
   module ApplicationHelper
+    def requeired_note
+      content_tag(:span) do
+
+      end
+    end
+
+    def requeired_note
+      content_tag(:span, t('helena.shared.required_indicator'), aria: { hidden: true }) +
+      content_tag(:span, t('helena.shared.sr_required_indicator'), class: 'sr-only')
+    end
+
+    def question_label(question)
+      content_tag(:label, for: "question_#{question.code}") do
+
+        raw(question.question_text) + (requeired_note if question.required?)
+      end
+    end
   end
 end
